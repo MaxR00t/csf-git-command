ab9eff43143ed8a3981bb90f162d0849beb16ad4
MaxR00t <15692039518@163.com>
2022-11-12 19:31:35 +0800

    update to master

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,7 @@
 public class Start {
     public static void main(String[] args) {
         System.out.println(" csf git command ");
-        String str = "max";
+        String str = "master";
         int a = 120;
     }
 }
