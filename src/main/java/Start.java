c8bd1c631d9b2c00579c9a8bd07024e0a00808f9
MaxR00t <102204033+MaxR00t@users.noreply.github.com>
2022-11-12 19:21:44 +0800

    Update Start.java

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,7 @@
 public class Start {
     public static void main(String[] args) {
         System.out.println(" csf git command ");
-        String str = "max";
+        String str = "max t s lan";
         int a = 120;
     }
 }
